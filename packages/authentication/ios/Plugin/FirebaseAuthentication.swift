15ece6e67429b7d6dfdfc2e9b3c611ac36e575bb
emilk <emilkaiser@gmail.com>
2022-06-14 11:10:17 +0200

    Create Twitter auth provider handler

--- packages/authentication/ios/Plugin/FirebaseAuthentication.swift
+++ packages/authentication/ios/Plugin/FirebaseAuthentication.swift
@@ -14,6 +14,7 @@ public typealias AuthStateChangedObserver = () -> Void
     private var gameCenterAuthProviderHandler: GameCenterAuthProviderHandler?
     private var facebookAuthProviderHandler: FacebookAuthProviderHandler?
     private var googleAuthProviderHandler: GoogleAuthProviderHandler?
+    private var twitterAuthProviderHandler: TwitterAuthProviderHandler?
     private var oAuthProviderHandler: OAuthProviderHandler?
     private var phoneAuthProviderHandler: PhoneAuthProviderHandler?
     private var savedCall: CAPPluginCall?
@@ -380,7 +381,7 @@ public typealias AuthStateChangedObserver = () -> Void
 
     @objc func signInWithTwitter(_ call: CAPPluginCall) {
         self.savedCall = call
-        self.oAuthProviderHandler?.signIn(call: call, providerId: ProviderId.twitter)
+        self.twitterAuthProviderHandler?.signIn(call: call)
     }
 
     @objc func signInWithYahoo(_ call: CAPPluginCall) {
@@ -539,6 +540,9 @@ public typealias AuthStateChangedObserver = () -> Void
         }
         if config.providers.contains(ProviderId.google) {
             self.googleAuthProviderHandler = GoogleAuthProviderHandler(self)
+        }
+        if config.providers.contains("twitter.com") {
+            self.twitterAuthProviderHandler = TwitterAuthProviderHandler(self)
         }
         if config.providers.contains(ProviderId.phone) {
             self.phoneAuthProviderHandler = PhoneAuthProviderHandler(self)
---
+++ packages/authentication/ios/Plugin/Handlers/TwitterAuthProviderHandler.swift
@@ -0,0 +1,61 @@
+import Foundation
+import Capacitor
+import FirebaseCore
+import FirebaseAuth
+
+class TwitterAuthProviderHandler: NSObject {
+    var pluginImplementation: FirebaseAuthentication
+    var provider: OAuthProvider?
+
+    init(_ pluginImplementation: FirebaseAuthentication) {
+        self.pluginImplementation = pluginImplementation
+    }
+
+    func signIn(call: CAPPluginCall) {
+        self.provider = OAuthProvider(providerID: "twitter.com")
+        self.applySignInOptions(call: call, provider: provider!)
+        DispatchQueue.main.async {
+            self.startSignInFlow()
+        }
+    }
+
+    private func applySignInOptions(call: CAPPluginCall, provider: OAuthProvider) {
+        let customParameters = call.getArray("customParameters", JSObject.self) ?? []
+        for (_, customParameter) in customParameters.enumerated() {
+            guard let key = customParameter["key"] as? String else {
+                continue
+            }
+            guard let value = customParameter["value"] as? String else {
+                continue
+            }
+            if provider.customParameters == nil {
+                provider.customParameters = [:]
+            }
+            provider.customParameters?[key] = value
+        }
+
+        let scopes = call.getArray("scopes", String.self) ?? []
+        provider.scopes = scopes
+    }
+
+    private func startSignInFlow() {
+        self.provider?.getCredentialWith(nil) { credential, error in
+            if let error = error {
+                self.pluginImplementation.handleFailedSignIn(message: nil, error: error)
+                return
+            }
+            if let credential = credential {
+
+                Auth.auth().signIn(with: credential) { (authResult, error) in
+                    if let error = error {
+                        self.pluginImplementation.handleFailedSignIn(message: nil, error: error)
+                        return
+                    }
+
+                    self.pluginImplementation.handleSuccessfulSignIn(credential: authResult?.credential, user: Auth.auth().currentUser, idToken: nil, nonce: nil, accessToken: nil)
+                }
+
+            }
+        }
+    }
+}
